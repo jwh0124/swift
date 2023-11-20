47d6c574b94004ebf3150f09c1d93dc6d9b6a3fa
jwh0124 <jwh0124@gmail.com>
2023-11-20 21:30:04 +0900

    create ios app initialized

---
+++ MyFirstApp/MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by 정원형 on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
