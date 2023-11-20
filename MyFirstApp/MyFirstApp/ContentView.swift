714fae9eb1e7a9da1f9eda053b428e92b3f31f54
jwh0124 <jwh0124@gmail.com>
2023-11-20 21:33:35 +0900

    update text

--- MyFirstApp/MyFirstApp/ContentView.swift
+++ MyFirstApp/MyFirstApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("저는 정원형입니다.")
         }
         .padding()
     }
