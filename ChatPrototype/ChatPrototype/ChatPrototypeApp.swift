18e63b5f871906bdddd245b863fd6b092ca5ff65
jwh0124 <jwh0124@gmail.com>
2024-07-26 15:13:07 +0900

    swift first tutorial

---
+++ ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by 정원형 on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
