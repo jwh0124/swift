b4c00ac0e491407ed4dd529b73b9985130f1b549
jwh0124 <jwh0124@gmail.com>
2024-07-28 17:31:03 +0900

    tutorial two - forecast prototype

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by 정원형 on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
