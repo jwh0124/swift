6c2ba2caaa901ae56225573c9bf47d76755e0e78
jwh0124 <jwh0124@gmail.com>
2024-08-01 13:50:47 +0900

    tutorial three - OnboardingFlow

---
+++ OnboardingFlow/OnboardingFlow/FeaturesPage.swift
@@ -0,0 +1,34 @@
+//
+//  FeaturesPage.swift
+//  OnboardingFlow
+//
+//  Created by 정원형 on 8/1/24.
+//
+
+import SwiftUI
+
+struct FeaturesPage: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Text("Features")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .fontWeight(.semibold)
+                .padding(.bottom)
+                .padding(.top, 100)
+            
+            FeatureCard(iconName: "person.2.crop.square.stack.fill", description: "A multiline description about a feature paired with the image on the left")
+            
+            FeatureCard(iconName: "quote.bubble.fill", description: "Short summary")
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FeaturesPage()
+        .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+        .background(Gradient(colors: gradientColors))
+        .foregroundStyle(.white)
+}
---
+++ OnboardingFlow/OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by 정원형 on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
